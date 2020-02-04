d99cebbae66bdaec66af083e2c64060af69d1d5c
Paulinely Morgan <paulinelym@gmail.com>
2020-02-03 21:12:46 -0300

    changes

--- estudo_forex/estudos.py
+++ estudo_forex/estudos.py
@@ -38,7 +38,7 @@ class Bar(object):
         return self.lixo
 
     def getId(self):
-        return self.getDate()+self.getHour()
+        return self.getDate() + " " + self.getHour()
 
 def loadBars(readFile):
 
@@ -85,18 +85,11 @@ def correlationBetweenTwo(a, b):
         verificacao necessária, pois há barras faltando (haverá comparação entre data e hora). 
         verifica qual par tem a primeira barra com data mais antiga. dai usa o par como laço externo.
     """
-    # d1 = datetime.strptime(a[0].getDate().replace('.', '-')+" "+a[0].getHour().split(':')[0]+":"+a[0].getHour().split(':')[1], "%Y-%m-%d %H:%M")
-    # d2 = datetime.strptime(b[0].getDate().replace('.', '-')+" "+b[0].getHour().split(':')[0]+":"+b[0].getHour().split(':')[1], "%Y-%m-%d %H:%M")
+    
     diff1 = today_time - getFormattedDate(a[0])
     diff2 = today_time - getFormattedDate(b[0])
 
-    if diff1 > diff2:
-        # começa por a (diff1)
-        return getCorrelation(a, b)
-    else:
-        # começa por b (diff2)
-        # diff2 maior ou diff2 = diff1
-        return getCorrelation(b, a)
+    return getCorrelation(a, b) if diff1 > diff2 else getCorrelation(b, a)
 
 bars_AUDUSD_M1_2018 = loadBars(data_AUDUSD_M1_2018)
 bars_EURUSD_M1_2018 = loadBars(data_EURUSD_M1_2018)
@@ -113,10 +106,8 @@ with open('correlation.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerows(row_list)
 
-plt.plot(x, y)
-plt.xlabel("Bars: {}".format(len(y)))
-plt.ylabel("Correlation")
-plt.title("Mean: {}".format(statistics.mean(y)))
-plt.show()
-
-
+# plt.plot(x, y)
+# plt.xlabel("Bars: {}".format(len(y)))
+# plt.ylabel("Correlation")
+# plt.title("Mean: {}".format(statistics.mean(y)))
+# plt.show()
